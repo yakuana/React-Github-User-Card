3ed0c3b6d2a41f77e318d78e7e559cb831f7c0aa
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-08-06 13:53:27 -0500

    WIP: Fetch github profile complete.

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    profile: {},
+  }
+
+  fetchProfile = () => {
+    fetch(`https://api.github.com/users/yakuana`)
+      // format the data.
+      .then(response => {
+        return response.json();
+      })
+
+      // set the state using the formatted data 
+      .then(userProfile => this.setState({ profile: userProfile }))
+
+      // check for unsuccessful fetch
+      .catch(error => {
+        console.log("The API is currently down.", error);
+      });
+  };
+
+  render() {
+
+    this.fetchProfile(); 
+
+    return (
+      <div className="App">
+        <h1>Hello!</h1>
+      </div>
+    )
+  } 
 }
 
 export default App;
